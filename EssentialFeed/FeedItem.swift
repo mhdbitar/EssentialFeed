2cf138902fcc6bd3b75f3bf1e2f067a66d3cf37f
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2021-11-01 22:59:09 +0300

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mohammad Bitar on 11/1/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUR: URL
+}
