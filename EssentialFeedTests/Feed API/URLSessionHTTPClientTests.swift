7ac43e167edb0e1ba852b0487747436aa0ff9e24
MHD <m.bitar@aalami.me>
2021-11-22 07:55:41 +0300

    Upgrade the Data to use the new version of it in Swift 5.5

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -151,7 +151,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     private func anyData() -> Data {
-        return Data(bytes: "any data".utf8)
+        return Data("any data".utf8)
     }
     
     private func anyNSError() -> NSError {
