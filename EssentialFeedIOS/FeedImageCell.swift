17759ce973a271a103c01af4da8be9aad5c951c2
MHD <m.bitar@aalami.me>
2022-01-06 08:45:56 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeedIOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Mohamd Bitar on 1/6/22.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
