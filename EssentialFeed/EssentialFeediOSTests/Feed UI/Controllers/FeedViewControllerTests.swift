dbae6842bfd136412c17f57d5484761bb4c2131c
MHD <m.bitar@aalami.me>
2022-01-17 08:01:37 +0300

    Localize feed view title string

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -13,7 +13,13 @@ final class FeedViewControllerTests: XCTestCase {
         let (sut, _) = makeSUT()
         
         sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.title, "My Feed")
+        
+        let bundle = Bundle(for: FeedViewController.self)
+        let localizedKey = "FEED_VIEW_TITLE"
+        let localizedTitle = bundle.localizedString(forKey: localizedKey, value: nil, table: "Feed")
+        
+        XCTAssertNotEqual(localizedKey, localizedTitle, "Missing localized sting for key: \(localizedKey)")
+        XCTAssertEqual(sut.title, localizedTitle)
     }
     
 	func test_loadFeedActions_requestFeedFromLoader() {
