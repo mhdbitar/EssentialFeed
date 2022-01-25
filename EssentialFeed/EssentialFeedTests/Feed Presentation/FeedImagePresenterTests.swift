8c7d72760cc33b5c87482d5d6ce1814ff2a2ad6a
MHD <m.bitar@aalami.me>
2022-01-25 08:38:38 +0300

    `FeedImagePresenter` does not send messages to view on init

---
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -0,0 +1,38 @@
+//
+//  FeedImagePresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Mohamd Bitar on 1/25/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class FeedImagePresenter {
+    init(view: Any) {
+        
+    }
+}
+
+class FeedImagePresenterTests: XCTestCase {
+    
+    func test_init_doesNotSendMessageToView() {
+        let (_, view) = makeSUT()
+        
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view message")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
+    private class ViewSpy {
+        let messages = [Any]()
+    }
+}
